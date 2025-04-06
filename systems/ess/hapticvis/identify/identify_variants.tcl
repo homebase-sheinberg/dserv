c92d9d5808873d7bbf19334b7421c60255f63512
homebase-sheinberg <homebase.sheinberg@gmail.com>
2025-04-05 20:52:00 -0400

    2 as ryan [author: ryan]

--- systems/ess/hapticvis/identify/identify_variants.tcl
+++ systems/ess/hapticvis/identify/identify_variants.tcl
@@ -6,6 +6,7 @@
 #   variant dictionary for visual and haptic identity learning
 #
 # 1
+# 2 as ryan
 #
 # Currently only supports 4, 6, 8 choices properly
 #   The "correct_choice" is numbered 1-n_choices
